88c243c5c1bd0db2a42fb42c25430330aae2be1f
Maikel Schuurmans <264235@student.fontys.nl>
2019-04-18 13:12:53 +0200

    More queries

--- LoginTest/LoginTest.DAL/Contexts/PreferenceContext.cs
+++ LoginTest/LoginTest.DAL/Contexts/PreferenceContext.cs
@@ -14,88 +14,6 @@ namespace Inzetsysteem.DAL.Contexts
         private readonly string _connectionString =
             "Server=mssql.fhict.local;Database=dbi389621;User Id=dbi389621;Password=Ensar123;";
 
-        public IEnumerable<OnderwijsTraject> GetAllOnderwijsTrajecten()
-        {
-            var trajecten = new List<OnderwijsTraject>();
-
-            using (SqlConnection connection = new SqlConnection(_connectionString))
-            {
-                connection.Open();
-
-                var sqlCommand = new SqlCommand($"select Id, Naam from Onderwijstraject", connection);
-                var reader = sqlCommand.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    trajecten.Add(new OnderwijsTraject
-                    {
-                        Id = (int) reader["Id"],
-                        Naam = reader["Naam"]?.ToString()
-                    });
-                }
-
-                connection.Close();
-            }
-
-            return trajecten;
-        }
-
-        public IEnumerable<OnderwijsTaak> GetTasksFromTraject(OnderwijsTraject onderwijsTraject)
-        {
-            var tasks = new List<OnderwijsTaak>();
-
-            using (SqlConnection connection = new SqlConnection(_connectionString))
-            {
-                connection.Open();
-
-                var sqlCommand = new SqlCommand($"SELECT a.Id, a.Naam FROM Onderwijstaak a \r\nINNER JOIN onderwijsonderdeel b ON a.OnderwijsonderdeelID = b.ID \r\nINNER JOIN Onderwijseenheid c ON b.OnderwijseenheidID = c.ID \r\nWHERE c.OnderwijstrajectID = '{onderwijsTraject.Id}'", connection);
-                var reader = sqlCommand.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    tasks.Add(new OnderwijsTaak
-                    {
-                        Id = (int)reader["Id"],
-                        Naam = reader["Naam"]?.ToString()
-                    });
-                }
-
-                connection.Close();
-            }
-
-            return tasks;
-        }
-
-        public IEnumerable<Preference> GetPreferencesFromTraject(OnderwijsTraject traject, int IdUser)
-        {
-            var preferences = new List<Preference>();
-            var tasks = GetTasksFromTraject(traject);
-
-            foreach (var task in tasks)
-            {
-                using (SqlConnection connection = new SqlConnection(_connectionString))
-                {
-                    connection.Open();
-
-                    var sqlCommand = new SqlCommand($"SELECT G.Naam, [Voorkeur waarde] FROM Voorkeuren\r\ninner join Gebruiker G on Voorkeuren.GebruikerID = G.ID\r\nWHERE OnderwijstaakID = {task.Id} \r\nand G.ID = {IdUser}", connection);
-                    var reader = sqlCommand.ExecuteReader();
-
-                    while (reader.Read())
-                    {
-                        preferences.Add(new Preference
-                        {
-                            Taak = task,
-                            Waarde = (int) reader["Voorkeur waarde"]
-                        });
-                    }
-
-                    connection.Close();
-                }
-            }
-
-            return preferences;
-        }
-
         public void SaveTrajectPreferences(IEnumerable<Preference> trajectPreferences, int userId)
         {
             foreach (var trajectPreference in trajectPreferences)
@@ -122,6 +40,255 @@ namespace Inzetsysteem.DAL.Contexts
                 }
             }
 
+        }
+
+        public IEnumerable<Preference> GetPreferencesFromTraject(OnderwijsTraject traject, int IdUser)
+        {
+            var preferences = new List<Preference>();
+            var tasks = GetTasksFromTraject(traject);
+
+            foreach (var task in tasks)
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    var sqlCommand = new SqlCommand($"SELECT G.Naam, [Voorkeur waarde] FROM Voorkeuren\r\ninner join Gebruiker G on Voorkeuren.GebruikerID = G.ID\r\nWHERE OnderwijstaakID = {task.Id} \r\nand G.ID = {IdUser}", connection);
+                    var reader = sqlCommand.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        preferences.Add(new Preference
+                        {
+                            Taak = task,
+                            Waarde = (int)reader["Voorkeur waarde"]
+                        });
+                    }
+
+                    connection.Close();
+                }
+            }
+
+            return preferences;
+        }
+
+
+
+
+
+
+
+
+
+
+        public IEnumerable<OnderwijsTraject> GetAllOnderwijsTrajecten()
+        {
+            var trajecten = new List<OnderwijsTraject>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand("GetAlleTrajecten", connection);
+
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    trajecten.Add(new OnderwijsTraject
+                    {
+                        Id = (int) reader["Id"],
+                        Naam = reader["Naam"]?.ToString()
+                    });
+                }
+
+                connection.Close();
+            }
+
+            return trajecten;
+        }
+
+        public IEnumerable<OnderwijsEenheid> GetAllOnderwijsEenheden(int trajectId)
+        {
+            var eenheden = new List<OnderwijsEenheid>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand("GetAlleEenheden", connection);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add(new SqlParameter("@TrajectID", trajectId));
+
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    eenheden.Add(new OnderwijsEenheid
+                    {
+                        Id = (int)reader["Id"],
+                        Naam = reader["Naam"]?.ToString()
+                    });
+                }
+
+                connection.Close();
+            }
+
+            return eenheden;
+        }
+
+        public IEnumerable<OnderwijsOnderdeel> GetAllOnderwijsOnderdelen(int EenheidId)
+        {
+            var onderdelen = new List<OnderwijsOnderdeel>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand("GetAlleOnderdelen", connection);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add(new SqlParameter("@EenheidID", EenheidId));
+
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    onderdelen.Add(new OnderwijsOnderdeel
+                    {
+                        Id = (int)reader["Id"],
+                        Naam = reader["Naam"]?.ToString()
+                    });
+                }
+
+                connection.Close();
+            }
+
+            return onderdelen;
+        }
+
+        public IEnumerable<OnderwijsTaak> GetAllOnderwijsTaken(int OnderdeelId)
+        {
+            var taken = new List<OnderwijsTaak>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand("GetAlleTaken", connection);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add(new SqlParameter("@OnderdeelID", OnderdeelId));
+
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    taken.Add(new OnderwijsTaak
+                    {
+                        Id = (int)reader["Id"],
+                        Naam = reader["Naam"]?.ToString()
+                    });
+                }
+
+                connection.Close();
+            }
+
+            return taken;
+        }
+
+        public IEnumerable<OnderwijsTaak> GetTasksFromTraject(OnderwijsTraject onderwijsTraject)
+        {
+            var tasks = new List<OnderwijsTaak>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand("GetTakenBinnenTraject", connection);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add(new SqlParameter("@TrajectID", onderwijsTraject.Id));
+
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    tasks.Add(new OnderwijsTaak
+                    {
+                        Id = (int)reader["Id"],
+                    });
+                }
+
+                connection.Close();
+            }
+
+            return tasks;
+        }
+
+        public IEnumerable<OnderwijsTaak> GetOnderdeelFromTraject(OnderwijsTraject onderwijsTraject)
+        {
+            var tasks = new List<OnderwijsTaak>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand("GetOnderdelenBinnenTraject", connection);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add(new SqlParameter("@TrajectID", onderwijsTraject.Id));
+
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    tasks.Add(new OnderwijsTaak
+                    {
+                        Id = (int)reader["Id"],
+                    });
+                }
+
+                connection.Close();
+            }
+
+            return tasks;
+        }
+
+        public IEnumerable<OnderwijsTaak> GetTasksFromEenheid(OnderwijsEenheid onderwijsEenheid)
+        {
+            var tasks = new List<OnderwijsTaak>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand("GetTakenBinnenEenheid", connection);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add(new SqlParameter("@EenheidID", onderwijsEenheid.Id));
+
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    tasks.Add(new OnderwijsTaak
+                    {
+                        Id = (int)reader["Id"],
+                    });
+                }
+
+                connection.Close();
+            }
+
+            return tasks;
         }
 
         public Preference CheckTaakPreference(OnderwijsTaak taak, int userId)
