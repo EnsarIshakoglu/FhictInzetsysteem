1a6914fc6f00733db794a2088ed06d72a7d19ce7
Maikel Schuurmans <264235@student.fontys.nl>
2019-05-09 19:15:36 +0200

    Made the code a bit more tidy

--- LoginTest/LoginTest.DAL/Contexts/IPreferenceContext.cs
+++ LoginTest/LoginTest.DAL/Contexts/IPreferenceContext.cs
@@ -6,7 +6,6 @@ namespace FHICTDeploymentSystem.DAL.Contexts
     public interface IPreferenceContext
     {
         IEnumerable<Section> GetAllSections();
-        void SaveEdSectionPreferences(IEnumerable<Preference> preferences, int userId);
         void AddTaskPreference(Task task, int priority, int userId);
         void UpdateTaskPreference(Task task, int priority, int userId);
         Preference CheckTaskPreference(Task task, int userId);
