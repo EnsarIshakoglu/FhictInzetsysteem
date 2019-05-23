fe31424ee58ac675e8e7ff2c54f08d1f01aed81e
Sanjay Soerdien <413271@student.fontys.nl>
2019-05-23 13:12:47 +0200

    Added console application

---
+++ Algorithm/Algorithm/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Visual studio addon voor git is veel beter!!
+                - Ensar 2019*/
+        }
+    }
+}
