db2a303824ce2923bcc1de487075e691d854d6d7
Maikel Schuurmans <264235@student.fontys.nl>
2019-06-03 15:57:59 +0200

    Added some more methodes to the algorithm

--- Algorithm/Algorithm/Logic/Logic.cs
+++ Algorithm/Algorithm/Logic/Logic.cs
@@ -13,7 +13,17 @@ namespace Algorithm
         public IEnumerable<EducationObject> AssignedTasks { get; private set; }
         public IEnumerable<Employee> Employees { get; private set; }
 
-        public void GetAllData()
+        public void StartAlgorithm()
+        {
+            GetAllData();
+            foreach (var task in AllTasks)
+            {
+                List<Employee> tempList = GetCompetentEmployees(task);
+                AddValueToEmployeePreferences(tempList);
+            }
+        }
+
+        private void GetAllData()
         {
             AllTasks = _context.GetAllTasks();
             Employees = _context.GetAllEmployees();
@@ -24,6 +34,39 @@ namespace Algorithm
             }
         }
 
+        private List<Employee> GetCompetentEmployees(EducationObject task)
+        {
+            List<Employee> tempList = new List<Employee>();
 
+            foreach (var employee in Employees)
+            {
+                if (CheckTaskCompetence(task, employee) || CheckUnitCompetence(task, employee) || CheckUnitExecCompetence(task, employee) || CheckSectionCompetence(task, employee))
+                {
+                    tempList.Add(employee);
+                }
+            }
+
+            return tempList;
+        }
+
+        private bool CheckTaskCompetence(EducationObject task, Employee employee)
+        {
+            return employee.Competences.Any(e => e.TaskId == task.Id);
+        }
+
+        private bool CheckUnitCompetence(EducationObject task, Employee employee)
+        {
+            return employee.Competences.Any(e => e.UnitId == task.UnitId);
+        }
+
+        private bool CheckUnitExecCompetence(EducationObject task, Employee employee)
+        {
+            return employee.Competences.Any(e => e.UnitTermExecId == task.UnitExecId);
+        }
+
+        private bool CheckSectionCompetence(EducationObject task, Employee employee)
+        {
+            return employee.Competences.Any(e => e.SectionId == task.SectionId);
+        }
     }
 }
